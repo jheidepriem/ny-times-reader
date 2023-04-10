9ed9ef50b9881ab63a03eef32a17abe8dab1c1e5
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-10 15:26:57 -0500

    Import api calls and add state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
+import { fetchData } from '../../../ApiCalls/Api';
 import './App.css';
 
 const App = () => {
+  const [articleData, setArticleData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    setLoading(true);
+    fetchData()
+      .then((data) => {
+        setLibraryData(data);
+        setLoading(false);
+      })
+      .catch((error) => setError(error));
+  }, []);
+
   return (
     <section className="article-container">
       <header className="home-title">
