9ba955ef004f21f9ba6732fa9cffa3c798ef44d7
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-10 18:15:26 -0500

    Add article to state

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,23 @@
-
 import "./Home.css";
-import Article from '../Article/Article';
+import Article from "../Article/Article";
 
-const Home = () => {
+const Home = ({ allArticles }) => {
   const mappedArticles = allArticles.map((article) => {
     return (
-      <Article/>
-
+      <Article
+        title={article.title}
+        byline={article.byline}
+        id={article.id}
+        key={article.id}
+        abstract={article.abstract}
+        uri={article.uri}
+        url={article.url}
+        image={article.multimedia[0].url}
+      />
     );
   });
 
-  return (
-   <section className="articles-container">{mappedArticles}</section>
-  );
+  return <section className="articles-container">{mappedArticles}</section>;
 };
 
-export default Library;
+export default Home;
