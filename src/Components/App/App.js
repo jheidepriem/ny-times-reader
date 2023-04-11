c36115c8aa0b9358555f28c68fdf55bf8239b2da
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-11 13:20:44 -0500

    Add filterArticles to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,8 @@
 import { fetchData } from '../../ApiCalls/Api';
+// import { Route, Switch } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import Home from '../Home/Home'
+import Form from '../Form/Form';
 import "../App/App.css"
 
 
@@ -8,6 +10,7 @@ const App = () => {
   const [articleData, setArticleData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('')
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     setLoading(true);
@@ -19,14 +22,25 @@ const App = () => {
       .catch((error) => setError(error));
   }, []);
 
+  const filterArticles = (searchValue) => {
+    const filteredData = articleData.filter((article) =>
+      article.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(filteredData);
+  };
+
   return (
+    <main className="App">
+ 
     <section className="app-container">
       <header className="home-title">
         <h1>New York Times Reader</h1>
+        <Form filterArticles={filterArticles}/>
         <Home allArticles={articleData}/>
-
-      </header>
+    </header>
     </section>
+
+    </main>
   );
 }
 
