3ade3d3a8ccfca52950853f5d2d3a05f32f4f321
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-11 14:13:42 -0500

    Add article details component

--- src/Components/ArticleDeets/ArticleDeets.js
+++ src/Components/ArticleDeets/ArticleDeets.js
@@ -1,13 +1,30 @@
-import '../ArticleDeets/ArticleDeets.css'
+import "../ArticleDeets/ArticleDeets.css";
+import { Link } from "react-router-dom";
 
-const ArticleDeets = () => {
-
-
+const ArticleDeets = ({ articleData }) => {
   return (
-    <main className='main-article-container'>
-      
+    <main className="main-article-container">
+      <h1>{articleData.title}</h1>
+      <p>{articleData.byline}</p>
+      <img
+        className="article-image"
+        src={articleData.multimedia[0].url}
+        alt="article image"
+      />
+      <section className="article-details-container">
+        <p>{articleData.published_date}</p>
+        <p>{articleData.section}</p>
+        <p>{articleData.abstract}</p>
+        <Link
+            to={{
+              pathname:{articleData.url},
+            }}
+            target="_blank"
+          >
+          </Link>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default ArticleDeets
+export default ArticleDeets;
