f3c7411983057a048c463ae6f3b8d935cabee988
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-11 13:20:54 -0500

    Edit spacing again

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,8 +1,6 @@
 // import { Link } from "react-router-dom";
 import "../Article/Article.css";
 
-
-
 const Article = ({ title, id, byline }) => {
   return (
    <section key={id} className="article-title">
