7962e736eed4b02176216f1e658437af6f30698c
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-11 22:01:52 -0500

    Fix spacing

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,17 +1,15 @@
-// import { Link } from "react-router-dom";
 import "../Article/Article.css";
 import { Link } from "react-router-dom";
 
 const Article = ({ title, byline }) => {
   return (
     <Link to={`article/${title}`} key={title}>
-   <section key={title} className="article-title">
-      <h1>{title}</h1>
-      <p>{byline}</p>
-    </section>
+      <section key={title} className="article-title">
+        <h1>{title}</h1>
+        <p>{byline}</p>
+      </section>
     </Link>
-    );
-
+  );
 };
 
 export default Article;
