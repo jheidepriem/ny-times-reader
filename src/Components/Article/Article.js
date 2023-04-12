c849a669efb19b433353ef2feb6647add25dfecb
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-12 09:58:14 -0500

    Add text container inside article container

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -5,8 +5,10 @@ const Article = ({ title, byline }) => {
   return (
     <Link to={`article/${title}`} key={title}>
       <section key={title} className="article-title">
+        <section className="article-title-text">
         <h1>{title}</h1>
         <p>{byline}</p>
+        </section>
       </section>
     </Link>
   );
